9553e691474f4e784c8687c103d9bf92dce6735f
gogi999 <g.petrovic9@gmail.com>
2023-01-23 18:35:02 +0100

    Created react app and added basic files and folders structure

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import { Box } from '@mui/material';
+
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from './components';
+
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: "#000" }}>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" exact element={<VideoDetail />} />
+                <Route path="/channel/:id" exact element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+);
+
+export default App;
