a7d6933285aac7e0a0623ee7fc003dbb80c4194a
gogi999 <g.petrovic9@gmail.com>
2023-01-24 19:20:51 +0100

    Started with UI of the app

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
+import { Search } from '@mui/icons-material';
+import {
+  IconButton,
+  Paper,
+} from '@mui/material';
+
 const SearchBar = () => {
     return (
-        <div>SearchBar</div>
+        <Paper
+            component="form"
+            onSubmit={() => {}}
+            sx={{ 
+                borderRadius: 20, 
+                border: "1px solid #e3e3e3",
+                pl: 2,
+                boxShadow: "none",
+                mr: { sm: 5 }
+            }}
+        >
+            <input 
+                type="text" 
+                className="search-bar"
+                placeholder="Search..."
+                value=""
+                onChange={() => {}}    
+            />
+            <IconButton
+                type="submit"
+                sx={{ p: "10px", color: "red" }}
+            >
+                <Search />
+            </IconButton>
+        </Paper>
     );
 }
 
