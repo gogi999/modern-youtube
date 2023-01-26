80e098598ba57005b9e9268278feb21f9bb57240
gogi999 <g.petrovic9@gmail.com>
2023-01-26 13:53:40 +0100

    Completed modern-youtube app

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,16 @@
-import CheckroomIcon from '@mui/icons-material/Checkroom';
-import CodeIcon from '@mui/icons-material/Code';
-import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
-import FaceRetouchingNaturalIcon
-  from '@mui/icons-material/FaceRetouchingNatural';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import HomeIcon from '@mui/icons-material/Home';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
-import SchoolIcon from '@mui/icons-material/School';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
